0895a314c88745db2fe44e7495c149b2d3dbeb51
CarlosPaz2020 <cpaz-i020k@kinal.edu.gt>
2020-05-26 20:00:13 -0600

    Modificando clase VentanaInstructorController.java desde SISTEMAS-DTG

--- kalum/src/main/java/edu/carlospaz/kalum/controllers/VentanaInstructorController.java
+++ kalum/src/main/java/edu/carlospaz/kalum/controllers/VentanaInstructorController.java
@@ -2,14 +2,12 @@ package edu.carlospaz.kalum.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.Initializable;
 
 public class VentanaInstructorController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
 
     }
     
