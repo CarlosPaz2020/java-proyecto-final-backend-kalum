993368e47cd2d548d732309f9bfc5574d9bfb111
CarlosPaz2020 <cpaz-i020k@kinal.edu.gt>
2020-05-27 00:35:57 -0600

    Agregando icono de la app

--- kalum/src/main/java/edu/carlospaz/kalum/App.java
+++ kalum/src/main/java/edu/carlospaz/kalum/App.java
@@ -8,7 +8,6 @@ import edu.carlospaz.kalum.controllers.VentanaPrincipalController;
 import edu.carlospaz.kalum.models.CarreraTecnica;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
@@ -27,7 +26,6 @@ public class App extends Application {
     public void start(Stage escenarioPrincipal) throws Exception {
         this.escenarioPrincipal = escenarioPrincipal;
         this.escenarioPrincipal.setTitle("Kalum v1.0.0");
-        this.escenarioPrincipal.getIcons().add(new Image("/img/icon_kalum_32x32.png"));
         mostrarVentanaPrincipal();
         this.escenarioPrincipal.show();
     }
